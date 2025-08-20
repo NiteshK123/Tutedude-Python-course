fa78eaff6f71e747592e75d6736527562c52bf7d
theycallmekurkure <113039244+NiteshK123@users.noreply.github.com>
2025-08-20 19:41:15 +0530

    Assignment 1

---
+++ Assignment 1.py
@@ -0,0 +1,13 @@
+
+# Task1 
+a = int(input('Enter the first number:'))
+b = int(input('Enter the second number:'))
+print('Addition: '+str(a+b))
+print('Subtraction: '+str(a-b))
+print('Multiplication: '+str(a*b))
+print('Division: '+str(a/b))
+
+#task2
+c = input('Enter your first name: ')
+d = input('Enter your last name: ')
+print('Hello,',c, d+'! Welcome to the Python program.')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tutedude-Python-course
 
+Assignment i -> refers to assignment number
+eg - Assignment 3 refers to assignment 3
