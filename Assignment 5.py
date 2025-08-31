a4b033474174acb057d323df21d6291e13a17c3b
theycallmekurkure <113039244+NiteshK123@users.noreply.github.com>
2025-08-31 20:34:49 +0530

    assignment 5 completed

--- Assignment 5.py
+++ Assignment 5.py
@@ -10,8 +10,9 @@ except:
 
 # Task 2
 l = [i+1 for i in range(10)]
+print('Orignal list: {}'.format(l))
 l1 = l[0:5]
-l2= l1
-l2.reverse()
-print(l1)
-print(l2)
+l2= l1[::-1]
+# l2.reverse()
+print('Extracted first five elements: {}'.format(l1))
+print('Reversed extracted list: {}'.format(l2))
